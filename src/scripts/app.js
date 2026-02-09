deed6707e5bdc23bf8a0b3b3c7062ba0df4b0ade
tikrack <tikrackcode@gmail.com>
2026-02-09 22:29:10 +0330

    finish

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,23 @@
 import "./tippy.js";
+
+const elm_input = document.querySelector("#input");
+const elm_output = document.querySelector("#output");
+
+elm_input.addEventListener("input", (e) => {
+  let text = e.target.innerText;
+
+  runCode(text);
+});
+
+const runCode = async (code) => {
+  const go = new Go();
+
+  go.argv = ["core.wasm", code];
+
+  const result = await WebAssembly.instantiateStreaming(
+    fetch("/core.wasm"),
+    go.importObject,
+  );
+
+  await go.run(result.instance);
+};
